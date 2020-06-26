10bbfe06eb85f51bb90760ffefc538a80969c991
Leonardo <leonardo_r@fastmail.com>
2020-06-26 13:02:39 -0300

    Implementação de um sistema de login.

--- src/Controle_Principal/Main.java
+++ src/Controle_Principal/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
         String nome, email, senha;
         int opcao_menu;
+        boolean senha_valida;
 
         Scanner scanner = new Scanner(System.in); // Armazena entrada do usuario
         ArrayList<Pedidos> listaPedidos = new ArrayList<>(); // Armazena os Pedidos
@@ -28,22 +29,52 @@ public class Main {
 
         Funcionario funcionario1 = new Funcionario(nome, email, senha);
 
-        opcao_menu = opcao_menu = Integer.parseInt(scanner.nextLine());
+        // Loop do Login
+        while(true) {
 
-        do {
-            switch (opcao_menu) {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                default:
-                    break;
+            // Loop do Menu
+
+            System.out.println("Bem-vindo(a) " + nome + ". Por favor, digite sua senha:");
+
+            // Verifica se a senha está correta. Prossegue para o programa de qualquer forma.
+            // Primeiramente gera o hash da senha e compara com o hash armazenado.
+
+            senha_valida = funcionario1.comparadorHash(funcionario1.geradorHash(scanner.nextLine()));
+
+            if(!senha_valida)
+            {
+                System.out.println("Atenção! Senha inválida. Deseja prosseguir mesmo assim?");
+                System.out.println("1. Sim\n2. Não");
+                opcao_menu = Integer.parseInt(scanner.nextLine());
+
+                if(opcao_menu != 1) continue;
             }
-        } while(opcao_menu != 4);
 
+            do {
+
+                opcao_menu = opcao_menu = Integer.parseInt(scanner.nextLine());
+
+                switch (opcao_menu) {
+                    case 1:
+
+                        if(!senha_valida)
+                        {
+                            System.out.println("Senha Inválida! Entre novamente.");
+                            break;
+                        }
+
+                        break;
+
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+                }
+            } while (opcao_menu != 4);
+        }
     }
 }
--- src/Enum/EstadoPedido.java
+++ src/Enum/EstadoPedido.java
@@ -4,7 +4,6 @@ package Enum;
  * Enum utilizado para armazenar a situação atual do pedido, utilizado pela classe Pedidos.
  */
 
-
 public enum EstadoPedido {
     REALIZADO, PREPARACAO, SAIU_PARA_ENTREGA, ENTREGUE, DEVOLVIDO
 }
