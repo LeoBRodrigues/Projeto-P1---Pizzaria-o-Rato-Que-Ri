76a88570fda409ea5b97773df832b69ae913bb98
Leonardo <leonardo_r@fastmail.com>
2020-06-26 12:21:15 -0300

    Descrição JavaDocs para os métodos implementados.

--- src/Controle_Funcionarios/Funcionario.java
+++ src/Controle_Funcionarios/Funcionario.java
@@ -6,6 +6,13 @@ public class Funcionario extends LoginSistema {
     private String nome, email, senha;
 
 
+    /**
+     * Construtor da classe Funcionario.
+     * @param nome (nome do funcionario a ser cadastrado)
+     * @param email (email do usuario a ser cadastrado)
+     * @param senha (senha de acesso do usuario a ser cadastrado, salva como Hash)
+     */
+
     public Funcionario (String nome, String email, String senha){
 
         this.nome = nome;
@@ -13,12 +20,24 @@ public class Funcionario extends LoginSistema {
         this.senha = geradorHash(senha);
     }
 
+    /**
+     * Gera um Hash de 32 bits para armazenar a senha de forma segura
+     * @param senha (String com a senha do usuario)
+     * @return (Retorna uma string com o hash gerado)
+     */
+
     @Override
     public String geradorHash(String senha){
 
         return Integer.toString(senha.hashCode());
 
     }
+
+    /**
+     * Compara o hash armazenado do usuário com a senha digitada na hora do login
+     * @param hashRecebido (Hash da senha digitada na hora do login)
+     * @return (Retorna True se os dois hashes forem iguais, False caso negativo)
+     */
 
     @Override
     public boolean comparadorHash(String hashRecebido){
