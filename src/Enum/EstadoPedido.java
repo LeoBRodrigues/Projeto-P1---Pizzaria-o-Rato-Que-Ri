4eb7c7ac45a77bee743b91da5b55dafcbef9a2af
Leonardo <leonardo_r@fastmail.com>
2020-06-26 12:10:00 -0300

    Enum para armazenar estado do pedido e forma de pagamento.

---
+++ src/Enum/EstadoPedido.java
@@ -0,0 +1,10 @@
+package Enum;
+
+/**
+ * Enum utilizado para armazenar a situação atual do pedido, utilizado pela classe Pedidos.
+ */
+
+
+public enum EstadoPedido {
+    REALIZADO, PREPARACAO, SAIU_PARA_ENTREGA, ENTREGUE, DEVOLVIDO
+}
---
+++ src/Enum/PagamentoPedido.java
@@ -0,0 +1,10 @@
+package Enum;
+
+/**
+ * Enum utilizado para armazenar a forma de pagamento a ser utilizada, utilizado pela classe Pedidos.
+ */
+
+
+public enum PagamentoPedido {
+    DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO
+}
