3b06451aa6e1654b6b6fc4508f59c5e8480aa752
Leonardo <leonardo_r@fastmail.com>
2020-06-26 12:41:30 -0300

    Cadastrando funcionario para teste.

--- src/Controle_Principal/Main.java
+++ src/Controle_Principal/Main.java
@@ -1,10 +1,20 @@
 package Controle_Principal;
 
+import Controle_Funcionarios.Funcionario;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        String nome, email, senha;
 
+        //Funcionario para Teste
+
+        senha = "123456";
+        nome = "Murilo Zanini";
+        email = "zanini@ORQR.com";
+
+        Funcionario funcionario1 = new Funcionario(nome, email, senha);
 
 
 
